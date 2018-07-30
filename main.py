fa490de7f1499ed1d4760bb354c1adde88243b89
Bakley <barclaykoin@gmail.com>
2018-07-30 13:13:58 +0300

    [CHORE #159395269]Added more functionality to the Menu file

--- main.py
+++ main.py
@@ -4,10 +4,57 @@ import sys
 class Menu:
     def __init__(self):
         pass
+        # self.comments = Comments()
+        # self.user = User()
+        # self.choices = {
+        #      "1": self.user
+        #     }
+
+    def display_users(self):
+        """This method displays the type of users we have"""
+        print("""
+        1. Normal User
+        2. Moderator
+        3. Admin
+        """)
 
     def display_menu(self):
-        pass
+        """This method displays the functionality of our app"""
+        print("""
+
+        """)
 
     def run(self):
         """Displays the Menu and respond to choices"""
-        pass
+        while True:
+            self.display_users()
+            # action = self.choices.get()
+            choose_a_user = input("Please log in as: ")
+            print(choose_a_user)
+            if action == "1":
+                print("""
+                a. Create a Comment
+                b. Edit a Comment
+                c. Show a Comment
+                """)
+                choose_a_method = input("What do you want to do?: ")
+            elif action == "2":
+                print("""
+                a. Create a Comment
+                b. Edit a Comment
+                c. Show a Comment
+                d. Delete a Comment
+                """)
+                choose_a_method = input("What do you want to do?: ")
+            else:
+                print("""
+                a. Create a Comment
+                b. Edit a Comment
+                c. Show a Comment
+                d. Delete a Comment
+                """)
+                choose_a_method = input("What do you want to do?: ")
+
+
+if __name__ == "__main__":
+    Menu().run()
